658700227876decf6395a2ee730d4e04530e937f
Nexor Hub <hercules4328@gmail.com>
2025-08-28 15:10:13 -0300

    Update Avisos.lua

--- Universal/Avisos.lua
+++ Universal/Avisos.lua
@@ -1,8 +1,1 @@
-local TextChatService = game:GetService("TextChatService")
-local mensagem1 = "Nexor Hub atualizado!"
-local mensagem2 = "Veja as novas funcionalidades em fixos, + Key fácil."
-pcall(function()
-    TextChatService.TextChannels.RBXGeneral:SendAsync(mensagem1)
-    task.wait(1) --mensagem para algo importante?
-    TextChatService.TextChannels.RBXGeneral:SendAsync(mensagem2)
-end)
+
