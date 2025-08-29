3f0cb985a465db9292ed3c96a141c2096a0a0bb7
Nexor Hub <hercules4328@gmail.com>
2025-08-28 23:43:38 -0300

    Update Avisos.lua

--- Universal/Avisos.lua
+++ Universal/Avisos.lua
@@ -1,1 +1,16 @@
+local TextChatService = game:GetService("TextChatService")
+local avisoFile = "aviso1.txt"
 
+local mensagem1 = "Primeira vez usando o Nexor Hub?"
+local mensagem2 = "Compatilhe o script com seus amigos!"
+
+pcall(function()
+    if not isfile(avisoFile) then
+        TextChatService.TextChannels.RBXGeneral:SendAsync(mensagem1)
+        task.wait(1)
+        TextChatService.TextChannels.RBXGeneral:SendAsync(mensagem2)
+        writefile(avisoFile, "executado")
+    else
+        warn("Aviso já foi executado, não será enviado novamente.")
+    end
+end)
