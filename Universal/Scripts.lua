d6234472a69becabe9762f37234e23320b69c479
Nexor Hub <hercules4328@gmail.com>
2025-11-04 14:08:58 -0300

    Update Scripts.lua

--- Universal/Scripts.lua
+++ Universal/Scripts.lua
@@ -1,11 +1,17 @@
-local url = "https://api.junkie-development.de/api/v1/luascripts/public/b1d8d974eaf51d928591b7b0dba54887404c1e14e868f52995a8d24b668ad890/download"
-local response = game:HttpGet(url)
-local fn, err = loadstring(response)
-if not fn then
-    warn("Erro ao compilar script remoto:", err)
-else
-    local ok, ret = pcall(fn)
-    if not ok then
-        warn("Erro ao executar script remoto:", ret)
+if getgenv().AlreadyExecutedtozx then return end
+getgenv().AlreadyExecutedtozx = true
+
+local target_url = "https://api.junkie-development.de/api/v1/luascripts/public/b1d8d974eaf51d928591b7b0dba54887404c1e14e868f52995a8d24b668ad890/download"
+
+task.spawn(function()
+    local queue_on_teleport = (syn and syn.queue_on_teleport) or queue_on_teleport or queueonteleport
+    if queue_on_teleport then
+        pcall(function()
+            queue_on_teleport('loadstring(game:HttpGet("' .. target_url .. '"))()')
+        end)
     end
-end
+end)
+
+pcall(function()
+    loadstring(game:HttpGet(target_url))()
+end)
