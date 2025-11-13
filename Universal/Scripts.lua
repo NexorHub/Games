ad94482a3207d7b97a162fe96da62c5baeba56b1
Nexor Hub <hercules4328@gmail.com>
2025-11-13 11:03:12 -0300

    Update Scripts.lua

--- Universal/Scripts.lua
+++ Universal/Scripts.lua
@@ -1,7 +1,7 @@
 if getgenv().AlreadyExecutedtozx then return end
 getgenv().AlreadyExecutedtozx = true
 
-local target_url = "https://raw.githubusercontent.com/NexorHub/Games/refs/heads/main/Universal/Sofware.lua"
+local target_url = "https://raw.githubusercontent.com/NexorHub/Games/refs/heads/main/Universal/Software.lua"
 
 task.spawn(function()
     local queue_on_teleport = (syn and syn.queue_on_teleport) or queue_on_teleport or queueonteleport
