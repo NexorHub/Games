fe1e0b72dacd8813c98d5a13669f7bfdc7a7d698
Nexor Hub <nexorhub3@gmail.com>
2025-12-05 14:54:19 -0300

    Update Scripts.lua

--- Universal/Scripts.lua
+++ Universal/Scripts.lua
@@ -2,7 +2,7 @@ if getgenv().AlreadyExecutedtozx then return end
 
 getgenv().AlreadyExecutedtozx = true
 
-local target_url = "https://scriptprotector.vercel.app/api/raw/667b632ee0919b68a54bf5ba96bb03b6"
+local target_url = "https://pastefy.app/aFEq9Y1f/raw"
 
 task.spawn(function()
 
